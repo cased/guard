a7f484bc677f96fa1f94d5f26a947d6cf68c6d10
tnm <ted@ted.io>
2021-01-26 12:12:40 -0800

    Version 0.5.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command.test import test as TestCommand
 
 setup(
     name="cased",
-    version="0.5.3",
+    version="0.5.4",
     description="Cased Guard adds security and visibility features to the command line tools you and your team love.",
     long_description="Cased Guard adds security and visibility features to the command line tools you and your team love. https://cased.com/guard",
     author="Cased",
