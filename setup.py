b4f80f337ffed79fa4a7b65b7c33c22635d407d9
tnm <ted@ted.io>
2021-04-23 10:19:49 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command.test import test as TestCommand
 
 setup(
     name="cased",
-    version="0.6.5",
+    version="0.6.6",
     description="Cased CLI adds security and visibility features to the command line tools you and your team love.",
     long_description="Cased CLI adds security and visibility features to the command line tools you and your team love. https://cased.com/guard",
     author="Cased",
